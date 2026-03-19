2982de7857d7c39411dd928403973807079f3452
Bùi Ngọc Bảo Trân <tranbnb.2004@gmail.com>
2026-03-19 16:10:32 +0700

    2

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import 'presentation/providers/product_provider.dart';
 import 'presentation/providers/cart_provider.dart';
 import 'presentation/providers/favorite_provider.dart';
+
 import 'presentation/screens/home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -20,6 +24,11 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
+        title: 'E-Commerce App',
+
+        // 🎨 nếu muốn giống design (dark)
+        theme: ThemeData.dark(),
+
         home: HomeScreen(),
       ),
     );
